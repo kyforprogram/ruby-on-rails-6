8034cbab971f4e346dc6647f9f01c6cdd181f8e3
EC2 Default User <kyforprogram@gamil.com>
2021-11-18 08:23:48 +0000

    Rspec

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -4,8 +4,8 @@ describe String do
   describe "#<<" do
     example "文字の追加" do
       s = "ABC"
-      s << "D"
-      # s << nil
+      # s << "D"
+      s << nil
       expect(s.size).to eq(4)
     end
   end
