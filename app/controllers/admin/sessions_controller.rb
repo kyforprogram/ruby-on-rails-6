f234efedd7705918729098248b61061f2238cb9f
EC2 Default User <kyforprogram@gamil.com>
2021-12-15 04:50:36 +0000

    p.182

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,43 +1,42 @@
-
-class Staff::SessionsController < Staff::Base
+class Admin::SessionsController < Admin::Base
 
   def new
-    if current_staff_member
-      redirect_to :staff_root
+    if current_administrator
+      redirect_to :admin_root
     else
-      @form = Staff::LoginForm.new
+      @form = Admin::LoginForm.new
       render action: "new"
     end
   end
 
   def create
-    # @form = Staff::LoginForm.new(params[:staff_login_form])
-    @form = Admin::LoginForm.new(params[:admin_login_form])
+    @form = Admin::LoginForm.new(login_form_params)
     if @form.email.present?
-      # staff_member = StaffMember.find_by(email: @form.email.downcase)
-      admin_member = Admin.find_by("(email)=?", @form.email.downcase)#これで通るのか後で試す
+      administrator =
+        Administrator.find_by("(email) = ?", @form.email.downcase)
     end
-    # if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-    if Admin::Authenticator.new(admin_member).authenticate(@form.password)
-      if administrator.suspended?#trueでアウト
-        flash.now.alert = "アカウントが停止されています"
+    if Admin::Authenticator.new(administrator).authenticate(@form.password)
+      if administrator.suspended?
+        flash.now.alert = "アカウントが停止されています。"
         render action: "new"
       else
-        session[:admme_member_id] = admin_member.id
-        flash.notice = "ログインしました"
+        session[:administrator_id] = administrator.id
+        flash.notice = "ログインしました。"
         redirect_to :admin_root
       end
     else
-      flash.now.alert = "メールアドレスまたはパスワードが正しくありません"
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
       render action: "new"
     end
   end
 
-  def destroy
-    session.delete(:admin_member_id)
-    flash.notice = "ログアウトしました"
-    redirect_to :admin_root
-
+  private def login_form_params
+    params.require(:admin_login_form).permit(:email, :password)
   end
 
+  def destroy
+    session.delete(:administrator_id)
+    flash.notice = "ログアウトしました。"
+    redirect_to :admin_root
+  end
 end
--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -4,7 +4,12 @@ class Admin::TopController < Admin::Base
   def index
     # raise IpAddressRejected
     # raise ActiveRecord::RecordNotFound
-    render action: "index"
+    # render action: "index"
+    if current_administrator
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 
 end
